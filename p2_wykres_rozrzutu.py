e2ad13278b1d01105144356edb7b4bd5861425ee
dzo4na <113539878+dzo4na@users.noreply.github.com>
2023-03-13 21:56:20 +0100

    2 - Scatterplot

---
+++ p2_wykres_rozrzutu.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#Wczytuję dane z pliku IRIS
+df = pd.read_csv("https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data",
+                 names = ["Długość kielicha", "Szerokość kielicha", "Długość płatka", "Szerokość płatka", "Klasa"])
+# df.head()
+# print(df)
+
+#Stworzenie list na podstawie danych z pliku IRIS
+df_dl_kielicha = df['Długość kielicha']
+dl_kielicha_lista = df_dl_kielicha.tolist()
+
+df_szer_kielicha = df['Szerokość kielicha']
+szer_kielicha_lista = df_szer_kielicha.tolist()
+
+df_dl_platka = df['Długość płatka']
+dl_platka_lista = df_dl_platka.tolist()
+
+df_szer_platka = df['Szerokość płatka']
+szer_platka_lista = df_szer_platka.tolist()
+
+#Stworzenie 4wykresów na rysunku i nadanie kolorów każdemu
+figure, ((axes1, axes2), (axes3, axes4)) = plt.subplots(2,2)
+figure.suptitle('Wykresy rozrzutu IRIS')
+axes1.scatter(dl_kielicha_lista,szer_kielicha_lista, color='blue')
+axes2.scatter(dl_platka_lista,szer_platka_lista, color='orange')
+axes3.scatter(dl_kielicha_lista,szer_platka_lista, color='red')
+axes4.scatter(dl_platka_lista,szer_kielicha_lista, color='green')
+plt.savefig('Rysunek - zadanie3.png', dpi=(200))
+plt.show()
